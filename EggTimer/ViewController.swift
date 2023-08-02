addd6f86a17d9ec87c2c56624e0e18806083d124
ViniciusCdeAlmeida <viniciusc.almeida1@gmail.com>
2023-08-01 21:25:47 -0300

    feat: contador funcional

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -10,6 +10,27 @@ import UIKit
 
 class ViewController: UIViewController {
     
-
-
+    @IBOutlet weak var titleLabel: UILabel!
+    let eggTimes = ["Soft":3,"Medium":7,"Hard":12]
+    var secondsRemaning = 60
+    var timer = Timer()
+    @IBAction func eggBtn(_ sender: UIButton) {
+        
+        timer.invalidate()
+        let hardness = sender.currentTitle!
+        
+        secondsRemaning = eggTimes[hardness]!
+        
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
+    }
+    
+    @objc func updateTimer(){
+        if secondsRemaning > 0{
+            secondsRemaning -= 1
+        } else {
+            timer.invalidate()
+            titleLabel.text = "Done!"
+        }
+    }
+    
 }
